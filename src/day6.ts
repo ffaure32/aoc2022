456010ea934b08d664362953b3c0a93feae64be0
ffaure32 <frederic.faure.partner@decathlon.com>
2022-12-06 19:25:59 +0100

    day 6 - improve oneliner

--- src/day6.ts
+++ src/day6.ts
@@ -1,6 +1,6 @@
 export function findUniqueCharsSequence(line: String, charsCount: number = 4): number {
     return ([...Array(line.length).keys()].
-        find((i) => new Set(line.slice(i, i + charsCount).split('')).size == charsCount)
+        find((i) => new Set(line.slice(i, i + charsCount)).size == charsCount)
         || -1)+charsCount;
 }
 
